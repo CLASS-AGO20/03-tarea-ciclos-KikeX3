3c7b2387dafb5fff3fd1f0f0197784dddb7bf133
KikeX3 <jguerrero24@ucol.mx>
2020-12-11 01:11:05 -0600

    Implemented sumatoriaSerieUno and sumatoriaSerieDos

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero){
+        let suma = 1;
+
+        for( let i = 2; i <= numero; i++){
+            suma = suma + 1/i;
+        }
+
+        return suma;
+    }
+
+    sumatoriaSerieDos(numero){
+        let suma = 1;
+        let i =2;
+        
+        while(i <= numero){
+            suma = suma + 1/i;
+            i++;
+            if (i % 2 !== 0){
+                suma = suma - 1/i;
+                i++;
+            }
+        }
+        return suma;
+    }
+}
 
 let app = new App();
+console.log(app.sumatoriaSerieUno(5));
+
+console.log(app.sumatoriaSerieDos(5));
